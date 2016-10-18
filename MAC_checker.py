58ab3c680ab99e3f635b5f4bd2652f2c12ee15f4
SamoJT <tomkinss@uni.coventry.ac.uk>
2016-10-18 18:20:56 +0100

    Add files via upload

--- MAC_checker.py
+++ MAC_checker.py
@@ -1,11 +1,29 @@
-macAddr_usrIn = input("Enter the first 3 bytes of the MAC address (ie AB:BC:CD): ")
+a = input("Is wireshark installed? y or n: ")
+print()
 
+if a == ("y"):
+    macAddr_usrIn = input("Enter the first 3 bytes of the MAC address (ie AB:BC:CD): ")
+    print()
+    manuf = open("/usr/share/wireshark/manuf",encoding="utf-8") 
+    for line in manuf:
+        if macAddr_usrIn in line:
+            print(line.encode("utf-8"))
 
-manuf = open("manuf.txt",encoding="utf-8")
-
-
-for line in manuf:
-
-    if macAddr_usrIn in line:
-        print(line.encode("utf-8"))
-
+elif a == ("n"): 
+    print("Please click this link to download the text file required for the program:")
+    print("https://raw.githubusercontent.com/SamoJT/MACchecker/master/manuf.txt")
+    print("Ensure it is in the same directory as this program.")
+    print("If you're unable to download, copy and paste into a text document and name manuf.txt")
+    print()
+    b = input("When downloaded and in same directory press y to continue: ")
+    if b == ("y"):
+        macAddr_usrIn = input("Enter the first 3 bytes of the MAC address (ie AB:BC:CD): ")
+        print()
+        manuf = open("manuf.txt",encoding="utf-8") 
+        for line in manuf:
+            if macAddr_usrIn in line:
+                print(line.encode("utf-8"))
+    else:
+        print("Invalid input, please restart program.")
+else:
+    print("Invalid input, please restart program.")
